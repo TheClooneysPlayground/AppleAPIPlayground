8d31bf426a257514552002c131d00b9c290f5e37
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2024-11-29 10:43:33 +1100

    Add UIKitPlayground

---
+++ UIKitPlayground/UIKitPlaygroundTests/UIKitPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitPlaygroundTests.swift
+//  UIKitPlaygroundTests
+//
+//  Created by Nicholas Clooney on 29/11/2024.
+//
+
+import Testing
+@testable import UIKitPlayground
+
+struct UIKitPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
